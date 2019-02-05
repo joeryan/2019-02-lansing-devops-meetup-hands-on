78c4a5245cd280995519fa294bae26d14a8fab54
Brendon Thiede <ThiedeB@courts.mi.gov>
2019-02-05 00:23:21 -0500

    Removing CORS package

--- Application/app.js
+++ Application/app.js
@@ -6,7 +6,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-const cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- scripts/Wait-SiteAvailable.ps1
+++ scripts/Wait-SiteAvailable.ps1
@@ -8,11 +8,18 @@ param (
   $SecondsBetweenAttempts = 30
 )
 
+Write-Verbose "Waiting for $WebAppUrl to be available"
 $attempts = 0
 do {
   $attempts += 1
-  $statusCode = (Invoke-WebRequest -Uri $WebAppUrl).StatusCode
+  Write-Verbose "Attempt $attempts calling $WebAppUrl"
+  $statusCode = (Invoke-WebRequest -Uri $WebAppUrl -ErrorAction SilentlyContinue).StatusCode
   if ($statusCode -ne 200) {
+    Write-Verbose "Received status code $statusCode. Sleeping for $SecondsBetweenAttempts seconds"
     Start-Sleep -Seconds $SecondsBetweenAttempts
   }
 } until ($statusCode -eq 200 -or $attempts -eq $MaximumAttempts)
+
+if ($statusCode -ne 200) {
+  exit 1
+}
