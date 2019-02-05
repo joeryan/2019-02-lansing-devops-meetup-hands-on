ff3ff99fa44dd89913a93548a6b78f19b38aad99
Brendon Thiede <ThiedeB@courts.mi.gov>
2019-02-05 00:24:02 -0500

    changing environment names

--- Application/routes/users.js
+++ Application/routes/users.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
-const envName = process.env['NODE_ENV'] || 'dev';
+const envName = process.env['NODE_ENV'] || 'development';
 const users = require(`../data/${envName}.users.json`)
 
 /* GET users listing. */
